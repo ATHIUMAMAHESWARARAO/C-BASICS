0ce7101802dd1831be1ce146752dcabbd133a49e
ATTI UMA MAHESWARA RAO <92287656+ATTIUMAMAHESWARARAO@users.noreply.github.com>
2021-10-20 10:49:29 +0530

    Add files via upload

---
+++ uma.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+	//variable declaration
+	int age;
+	float cgpa;
+	char initial;
+	
+	//initiallization
+	age = 20;
+	cgpa = 8.29;
+	initial = 'A';
+	
+	//output
+	printf("my age is %i\n", age);
+	printf("my cgpa is %f\n", cgpa);
+	printf("my initial is %c", initial);	
+}
