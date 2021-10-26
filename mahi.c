4fd093fc8745ed5c818d65dd2d7e81932ccaee85
ATTI UMA MAHESWARA RAO <92287656+ATTIUMAMAHESWARARAO@users.noreply.github.com>
2021-10-26 10:27:23 +0530

    Add files via upload

---
+++ mahi.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+	int age;
+	float cgpa;
+	char initial;
+	
+	age = 19;
+	cgpa = 8.29;
+	initial = 'A';
+	
+	printf("my age is %d\n", age);
+	printf("my cgpa is %f\n", cgpa);
+	printf("my initial starts with is %c", initial);
+	
+}
