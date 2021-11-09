0d4b6586a2906dac376a152f1d58b3ffea14b544
ATTI UMA MAHESWARA RAO <92287656+ATTIUMAMAHESWARARAO@users.noreply.github.com>
2021-11-09 10:28:29 +0530

    Add files via upload

---
+++ scanf-1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+	int a, b, c;
+	printf("enter three number");
+	scanf("%d%d%d", &a, &b, &c);
+	printf("number1:%d\n", a);
+	printf("number2:%d\n", b);
+	printf("number3:%d\n", c);
+}
