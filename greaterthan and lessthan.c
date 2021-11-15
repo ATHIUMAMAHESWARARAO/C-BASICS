f84a9c8400108fcf399ee433696f76fbe085a7cf
ATTI UMA MAHESWARA RAO <92287656+ATTIUMAMAHESWARARAO@users.noreply.github.com>
2021-11-15 10:34:08 +0530

    Add files via upload

---
+++ greaterthan and lessthan.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+	int a,b;
+	a=20;
+	b=5;
+	printf("%d > %d--> %d\n",a,b, a>b);
+	printf("%d >= %d--> %d\n",a,b, a>=b);
+	printf("%d < %d--> %d\n",a,b, a<b);
+	printf("%d <= %d--> %d\n",a,b, a<=b);
+	printf("%d == %d--> %d\n",a,b, a==b);
+	printf("%d != %d--> %d",a,b, a!=b);
+}
---
+++ logical operations.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main()
+{
+	printf("%d\n", (16>2) && (18>2));
+	printf("%d\n", (16>4) && (16>8));
+    printf("%d\n", (47<4) || (43>5));
+	printf("%d\n", (95>5) || (77<3));
+}
