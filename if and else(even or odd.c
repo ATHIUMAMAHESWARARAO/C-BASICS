57605559811a499eb30b373800e543f82c971f82
ATTI UMA MAHESWARA RAO <92287656+ATTIUMAMAHESWARARAO@users.noreply.github.com>
2021-12-06 10:47:44 +0530

    Add files via upload

---
+++ if and else(even or odd.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+	int n;
+	printf("even or odd");
+	scanf("%d", &n);
+	if (n%2 ==0)
+	{
+		printf("%d is even", n);	
+	}
+	
+	else
+    {
+    	printf("%d is odd");
+	}
+}
