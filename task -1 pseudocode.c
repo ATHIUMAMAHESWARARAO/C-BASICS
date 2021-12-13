0c8ce7ed0719aa8d1258bd96ac227c96cf3d7a62
ATTI UMA MAHESWARA RAO <92287656+ATTIUMAMAHESWARARAO@users.noreply.github.com>
2021-12-13 10:49:48 +0530

    Add files via upload

---
+++ task -1 pseudocode.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+	int p, q, r;
+    printf("enter a number");
+	scanf("%d%d%d", &p, &q, &r); 
+	if((r>p) || (7-7)>(r-7))
+	{
+		 q=p;
+	}
+	else
+	{
+		 p=12;
+	}
+		printf("the answer is %d", p+q+r);
+}
