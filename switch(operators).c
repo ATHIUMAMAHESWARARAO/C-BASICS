68f9b1da7a291ba9e2f5d553b5f1314a62610a11
ATTI UMA MAHESWARA RAO <92287656+ATTIUMAMAHESWARARAO@users.noreply.github.com>
2021-12-15 10:54:05 +0530

    Add files via upload

---
+++ switch(operators).c
@@ -0,0 +1,75 @@
+#include<stdio.h>
+void main()
+{
+	int choice, ac, rc, bc;
+	int a, b;
+	printf("enter your number"); 
+	scanf("%d%d", &a, &b);
+	printf("enter your choice:\n1. Arthematic Operators\n2. Relative Operators\n3. Bitwise Operators");
+	scanf("%d", choice);
+	switch(choice)
+	{
+		case 1:
+			printf("enter your choice\n1. +\n2. -\n3. *\n4. /\n5. %%");
+			scanf("%d", ac);
+			switch(ac)
+			{
+				case 1:
+					printf("%d + %d = %d", a, b, a+b);
+					break;
+				case 2:
+					printf("%d - %d = %d", a, b, a-b);
+					break;
+				case 3:
+					printf("%d * %d = %d", a, b, a*b);
+					break;
+				case 4:
+					printf("%d / %d = %d", a, b, a/b);
+					break;
+				case 5:
+					printf("%d %% %d = %d", a, b, a%b);
+					break;
+			}
+		
+		case 2:
+		    printf("enter your choice\n1. >\n2. >=\n3. <\n4. <=\n5. ==\n6. !=");
+			scanf("%d", rc);
+			switch(rc)
+			{
+				case 1:
+					printf("%d > %d = %d", a, b, a>b);
+					break;
+				case 2:
+					printf("%d >= %d = %d", a, b, a>=b);
+					break;
+				case 3:
+					printf("%d < %d = %d", a, b, a<b);
+					break;
+				case 4:
+					printf("%d <= %d = %d", a, b, a<=b);
+					break;
+				case 5:
+					printf("%d == %d = %d", a, b, a==b);
+					break;
+				case 6:
+				    printf("%d != %d = %d", a, b, a!=b);
+				    break;
+			}	
+			
+		case 3:
+			printf("enter your choice\n1. &\n2. |\n3. ^");
+			scanf("%d", bc);
+			switch(bc)
+			{
+				case 1:
+					printf("%d & %d = %d", a, b, a&b);
+					break;
+				case 2:
+					printf("%d | %d = %d", a, b, a|b);
+					break;
+				case 3:
+					printf("%d ^ %d = %d", a, b, a^b);
+					break;	
+			}
+    }
+}
